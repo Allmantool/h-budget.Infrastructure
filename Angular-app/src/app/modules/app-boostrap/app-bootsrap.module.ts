0a59c7c63b2dd9db9016742ecd165b7e28334fc1
GODELTECH\p.chyzhykau <pavel.chyzhykau@sorted.com>
2022-07-07 12:49:09 +0300

    chore(M3): fix docker compose

--- Angular-app/src/app/modules/app-boostrap/app-bootsrap.module.ts
+++ Angular-app/src/app/modules/app-boostrap/app-bootsrap.module.ts
@@ -3,15 +3,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from "@angular/core";
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { AppBootsrapRoutingModule, AppRootComponent } from '../app-boostrap';
 import { AppSharedModule } from './../shared/shared.module';
+import { ngxsConfig } from './../shared/store/ngxs.config';
 
 @NgModule({
     declarations: [
         AppRootComponent
     ],
     imports: [
+        NgxsModule.forRoot([], ngxsConfig),
+        NgxsLoggerPluginModule.forRoot(),
+		NgxsReduxDevtoolsPluginModule.forRoot(),
         AppSharedModule,
         CommonModule,
         HttpClientModule,
--- Angular-app/src/app/modules/main-dashboard/main-dashboard.module.ts
+++ Angular-app/src/app/modules/main-dashboard/main-dashboard.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { NgxsModule } from '@ngxs/store';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 import { AppSharedModule } from './../shared/shared.module';
-import { environment } from '../../../environments/environment.prod';
 import { CurrencyRatesState } from '../shared/store/states/currency-rates.state';
 import { MainDashboardRoutingModule } from './main-dashboard-routing.module';
 import { MainDashboardComponent } from './components/dashboard/main-dashboard.component';
@@ -16,11 +13,7 @@ import { MainDashboardCartComponent } from './components/dashboard-item/main-das
 		MainDashboardCartComponent
 	],
 	imports: [
-		NgxsModule.forRoot([CurrencyRatesState], {
-			developmentMode: !environment.production,
-		}),
-		NgxsLoggerPluginModule.forRoot(),
-		NgxsReduxDevtoolsPluginModule.forRoot(),
+		NgxsModule.forFeature([CurrencyRatesState]),
 		MainDashboardRoutingModule,
 		AppSharedModule,
 	],
