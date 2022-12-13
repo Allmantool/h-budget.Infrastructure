d654d3c3080fb3c3e623972977a02a5766e7fd07
GODELTECH\p.chyzhykau <pavel.chyzhykau@sorted.com>
2022-12-13 20:26:19 +0300

    feat(M3): add kibana image

---
+++ WebApi-app/HomeBudget-Web-API/HomeBudget.Components.IntegrationTests/CurrencyRatesControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace HomeBudget.Components.IntegrationTests
+{
+    [TestFixture]
+    public class CurrencyRatesControllerTests
+    {
+        [Test]
+        [Ignore("Intend to be used only for local testing. Not appropriate infrastructure has been setup")]
+        public async Task GetTodayRatesForPeriodAsync_When_Then()
+        {
+            var webAppFactory = new HomeBudgetWebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateClient();
+
+            const string bodyAsJson = "";
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "/currencyRates/period")
+            {
+                Content = new StringContent(bodyAsJson, Encoding.UTF8, "application/json")
+            };
+
+            _ = await httpClient.SendAsync(request);
+        }
+    }
+}
