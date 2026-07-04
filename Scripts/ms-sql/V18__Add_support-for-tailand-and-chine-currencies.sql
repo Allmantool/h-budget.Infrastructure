b6bd22916e8e04be6ad6bf5f7cae13b91d5dbbfa
Pavel Chyzhykau <allmantool@gmail.com>
2026-07-04 14:47:06 +0300

    chore: fix issue with new currancies setup (#496)
    Co-authored-by: ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>

--- Scripts/ms-sql/V18__Add_support-for-tailand-and-chine-currencies.sql
+++ Scripts/ms-sql/V18__Add_support-for-tailand-and-chine-currencies.sql
@@ -1,30 +1,62 @@
-USE [HomeBudget.Accounting];
+USE [HomeBudget.CurrencyRates];
 GO
 
-DECLARE @GeneralKey AS NVARCHAR(15) = N'General';
+SET XACT_ABORT ON;
 
-DECLARE @defaultSettings AS NVARCHAR(MAX) = 
-    N'{' +
-        N'"ActiveNationalBankCurrencies" :' +
-            N'[' +
-                N'{"Abbreviation":"USD", "Id":431, "Name": "US Dollar", "Scale": 1},' +
-                N'{"Abbreviation":"RUB", "Id":456, "Name": "Russian Ruble", "Scale": 100},' +
-                N'{"Abbreviation":"EUR", "Id":451, "Name": "Euro", "Scale": 1},' +
-                N'{"Abbreviation":"UAH", "Id":449, "Name": "Hryvnia", "Scale": 100},' +
-                N'{"Abbreviation":"PLN", "Id":452, "Name": "Zloty", "Scale": 10},' +
-                N'{"Abbreviation":"TRY", "Id":460, "Name": "Turkish Lira", "Scale": 10},' +
-                N'{"Abbreviation":"CNY", "Id":462, "Name": "Yuan Renminbi", "Scale": 10},' +
-                N'{"Abbreviation":"THB", "Id":468, "Name": "Baht", "Scale": 100}' +
-            N']' +
-    N'}';
+BEGIN TRY
+    BEGIN TRANSACTION;
 
-IF EXISTS (SELECT 1 FROM dbo.ConfigSettings WITH (NOLOCK) WHERE [Key] = @GeneralKey)
-BEGIN
+    DECLARE @GeneralKey AS NVARCHAR(15) = N'General';
+
+    DECLARE @defaultSettings AS NVARCHAR(MAX) = 
+        N'{' +
+            N'"ActiveNationalBankCurrencies" :' +
+                N'[' +
+                    N'{"Abbreviation":"USD", "Id":431, "Name": "US Dollar", "Scale": 1},' +
+                    N'{"Abbreviation":"RUB", "Id":456, "Name": "Russian Ruble", "Scale": 100},' +
+                    N'{"Abbreviation":"EUR", "Id":451, "Name": "Euro", "Scale": 1},' +
+                    N'{"Abbreviation":"UAH", "Id":449, "Name": "Hryvnia", "Scale": 100},' +
+                    N'{"Abbreviation":"PLN", "Id":452, "Name": "Zloty", "Scale": 10},' +
+                    N'{"Abbreviation":"TRY", "Id":460, "Name": "Turkish Lira", "Scale": 10},' +
+                    N'{"Abbreviation":"CNY", "Id":462, "Name": "Yuan Renminbi", "Scale": 10},' +
+                    N'{"Abbreviation":"THB", "Id":468, "Name": "Baht", "Scale": 100}' +
+                N']' +
+        N'}';
+
+    IF ISJSON(@defaultSettings) <> 1
+    BEGIN
+        THROW 51000, 'Invalid General ConfigSettings JSON.', 1;
+    END;
+
     DELETE FROM dbo.ConfigSettings
     WHERE [Key] = @GeneralKey;
-END
 
-INSERT INTO dbo.ConfigSettings
-            ( [Key], Settings )
-     VALUES ( @GeneralKey, @defaultSettings );
+    INSERT INTO dbo.ConfigSettings ([Key], Settings)
+    VALUES (@GeneralKey, @defaultSettings);
+
+    INSERT INTO dbo.CurrencyAbbreviations ([CurrencyId], [Name], [Abbreviation])
+    SELECT V.CurrencyId, V.[Name], V.Abbreviation
+    FROM
+    (
+        VALUES
+            (462, N'Китайский юань', N'CNY'),
+            (468, N'Таиландский бат', N'THB')
+    ) AS V([CurrencyId], [Name], [Abbreviation])
+    WHERE NOT EXISTS
+    (
+        SELECT 1
+        FROM dbo.CurrencyAbbreviations AS CA
+        WHERE CA.CurrencyId = V.CurrencyId
+    );
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK TRANSACTION;
+    END;
+
+    THROW;
+END CATCH;
 GO
